54f20ce7fe582553bd029d9af4902814dcf219fe
Aziz <contactzdevapp@gmail.com>
2025-11-04 08:57:25 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,29 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('jadwal-cache-v1').then(cache => {
+    caches.open('jadwal-cache-v2').then(cache => {
       return cache.addAll([
         '/',
         '/index.html',
         '/style.css',
         '/script.js',
-        '/images.png'
+        '/images.png',
+        '/offline.html',
+        '/offline.svg'
       ]);
     })
   );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match('/offline.html'))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  }
 });
