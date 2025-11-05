648b43d6aae11ea0779ac6ea4dc234b99ff3b34b
Aziz <contactzdevapp@gmail.com>
2025-11-05 16:16:00 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,21 @@
-const CACHE_NAME = "jadwal-cache-v2";
-const ASSETS = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/icon.png",
-  "/splash.png"
-];
-
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open('jadwal-cache-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/images.png'
+      ]);
+    })
   );
-  self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(resp => 
-      resp || fetch(event.request).then(fetchResp => {
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, fetchResp.clone());
-          return fetchResp;
-        });
-      })
-    ).catch(() => caches.match("/index.html"))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
