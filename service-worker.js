a823db8bbb78528521d7685964b64d696918eb8b
Aziz <contactzdevapp@gmail.com>
2025-11-06 14:24:40 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,7 @@
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('jadwal-cache-v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js',
-        '/images.png'
-      ]);
-    })
-  );
+  console.log('Service Worker aktif tanpa cache.');
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
